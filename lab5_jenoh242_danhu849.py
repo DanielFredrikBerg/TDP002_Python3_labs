80f3f3884796add133e249a7aea7530a01b7c086
jenoh242 <jenoh242@student.liu.se>
2020-10-02 23:15:10 +0200

    Finnished lab5.

--- lab5_jenoh242_danhu849.py
+++ lab5_jenoh242_danhu849.py
@@ -1,113 +1,166 @@
+#!/usr/bin/env python3
 import os
 
+#jenoh242 & danhu849, 2020-10-02
+
 def main():
-	assignments = []
-			
 	while True:
-		choice = input("Vilken funktion vill du köra? \n 5a : 1 \n 5b : 2 \n 3  : 5c \n 5d : 4 \n 5e : 5 \n 5f : 6 \n 5h : 7")
-		eval(assignments[choice])
+		try:
+			print("##########################################################")
+			print("#Skriv 'a'-'h' för en uppgift.                           #")
+			print("#Skriv 'exit', 'quit' eller 'q' för att avsluta.         #")
+			print("##########################################################")
+			choice = input("\n#Uppgift 5")
+			print()
+			if choice.lower() in ["exit", "quit", "q"]:
+				break
+			elif choice.lower() not in [chr(i) for i in range(97, 105)]:
+				print("Inte en uppgift!")
+			else:
+				eval("assignment_" + choice.lower() + "()")
+				print()
+		except Exception as e:
+			print(e)
 
-	
+##########assignments start here##########
 
-#####################################################################################################
 #5a
-def a():
-	summa = lambda x: x + summa(x-1) if (x > 0) else 0
-	prod = lambda x: x * prod(x-1) if (x > 1) else 1
-	print(somefunc(summa,512))
-	print(somefunc(prod,512))
+#Tar en funktion och ett tal, och kallar funktionen med talet.
 def somefunc(func, tal):
 	return func(tal)
+
+def assignment_a():
+	summa = lambda x: x + summa(x-1) if (x > 0) else 0
+	prod = lambda x: x * prod(x-1) if (x > 1) else 1
+	add = int(input("Summera up till vilket tal?"))
+	print("Summan av alla tal från 0 till ", add,":\n", somefunc(summa, add), "\n")
+	times = int(input("Multiplicera upp till vilket tal?"))
+	print("Produkten av alla tal från 1 till ", times,":\n", somefunc(prod,times))
+
 #5b
-def b():
+#För varje person, kolla om söksträngen är finns i värdet av det angivna fältet
+#Returnerar persons uppgifter om så är fallet.
+def search_db(db, search_field, search_string):
+	search_results = []
+	for pers in db:
+		if  search_string.lower() in pers[search_field].lower():
+			search_results.append(pers)
+	return search_results
+
+def assignment_b():
 	db = [
 	{'name': 'Jakob', 'position': 'assistant'},
 	{'name': 'Åke', 'position': 'assistant'},
 	{'name': 'Ola', 'position': 'examiner'},
 	{'name': 'Henrik', 'position': 'assistant'}
 	]
-	print(search_db(db, 'position', 'examiner'))
-def search_db(db, search_field, search_string):
-	search_results = []
-	for dic in db:
-		if dic[search_field] == search_string:
-			search_results.append(dic)
-	return search_results
-#5c
-def c():
-	haystack = 'Can you find the needle in this haystack?'.split()
-	print(contains('needle', haystack))
-	print(contains2('needle', haystack))
-	print(contains3('needle', haystack))
+	print("Databasen:\n", db, "\n")
+	field = input("Vilket fält vill du söka på?\n")
+	string = input("Vilket ord vill du söka på?\n")
+	print("Svar på sökning: ", search_db(db, field, string))
 
+#5c
+#Använder "__contains__" för att kolla om ordert finns i listan.
 def contains(word, haystack):
 	return haystack.__contains__(word)
 
+#Använder en "list comprehension" som fylls med instancer av det funna ordet.
+#Om listan innehåller något, returnera "True".
 def contains2(word, haystack):
 	found = [oword for oword in haystack if oword == word]
 	return bool(found)
 
+#Samma som ovan fast ett lambda uttryck.
 contains3 = lambda word, haystack: bool([oword for oword in haystack if oword == word])
 
+def assignment_c():
+	haystack = 'Can you find the needle in this haystack?'.split()
+	print('Finns "needle" i ', haystack, "?\n")
+	print("----------------------------------------------------------")
+	print('|Med "__contains__"                               |' , contains('needle', haystack), "|")
+	print("|--------------------------------------------------------|")
+	print('|Med "list comprehension"                         |' , contains2('needle', haystack), "|")
+	print("|--------------------------------------------------------|")
+	print('|Med "List comprehension", fast i lambda funktion |' , contains3('needle', haystack), "|")
+	print("----------------------------------------------------------")
+
 #5d
-def d():
-	while(True):
-		try:
-			command = input("command> ")
-			command = command.split(' ')
-			if len(command) > 1:
-				eval(command[0] + '("' + command[1] + '")')
-			elif command[0] == "q":
-				break
-			else:
-				eval(command[0] + '()')
-		except Exception:
-			print('No such command')
-		
-def cat_func(path):
-	with open('path', 'r') as content:
-		return content
-def pwd():
+def command_pwd():
 	print(os.getcwd())
-def ls(path = '.'):
+
+#Tar in en sökväg till en katalog, och listar innehållet för.
+#Om inget anges används katalogen användaren står i.
+def command_ls(path = '.'):
 	for file_name in os.listdir(path):
 		print(file_name)
-def cd(path = '.'):
+
+#Hittar sökvägen till nuvarande katalog, och skriver ut den.
+def command_cd(path = '.'):
 	os.chdir(path)
-def cat(path):
+
+#Öppnar en fil, och skriver ut innehållet.
+def command_cat(path):
 	if path:
 		with open(path, 'r') as content:
 			print(content.read())
 	else:
 		print('Please add args for cat')
 
+def assignment_d():
+	print('Kommandon: pwd, ls, cd, cat')
+	print('Skriv "exit", "quit" eller "q" för att avsluta.')
+	while(True):
+		try:
+			command = input("command> ")
+			command = command.split(' ')
+			if command[0].lower() in ["q", "exit", "quit"]:
+				break
+			elif len(command) > 1:
+				eval("command_" + command[0] + '("' + command[1] + '")')
+			else:
+				eval("command_" + command[0] + '()')
+		except Exception:
+			print('No such command')
 
 #5e
-def stars(n): return '*' * n
-gen_list = lambda func, integer: [func(variable) for variable in range(1, integer +1)]
-print(gen_list(stars, 3))
+stars = lambda n : '*' * n
+
+#Genererar en n lång lista med hjälp av en funktion.
+generate_list = lambda func, n: [func(variable) for variable in range(1, n+1)]
+
+def assignment_e():
+	ammount = int(input("Hur många stjärnor? "))
+	print(generate_list(stars, ammount))
 
 #5f
 add = lambda x, y: x + y
 
+#Skapar en "förifylld" funktion.
 def partial(func, condition):
 	return lambda value: func(condition, value)
 
-add_five = partial(add, 5)
-print(add_five(3))
+def assignment_f():
+	print("Addera två siffror.")
+	num = int(input("skriv det första nummret: "))
+	add_num= partial(add, num)
+	num = int(input("Skriv det andra nummret: "))
+	print(add_num(num))
 
 #5g
 m_5 = lambda x: x * 5
 a_10 = lambda x: x + 10
 
+#Skapar en funktion av en funktion i en funktion.
 def compose(F_a, F_b):
 	return lambda x: F_a(F_b(x))
 	
-composition = compose(a_10, m_5)
-print(composition(3))
+def assignment_g():
+	x = int(input("f(x) = (x * 5) + 10\nSkriv nummer för x: "))
+	composition = compose(a_10, m_5)
+	print(composition(x))
 
 #5h
-
+#Skapar en ny funktion av två funktioner.
 def make_filter_map_test(filt_func, pow_func):
 	def new_func(lista):
 		ue_filter_func = partial(filter, filt_func)
@@ -116,7 +169,11 @@ def make_filter_map_test(filt_func, pow_func):
 		return list(multiplied_func(lista))
 	return new_func
 
-def make_filter_map_test2(filt_func, pow_func): return lambda l: list(compose(partial(filter, filt_func), partial(map, pow_func)))
+def assignment_h():
+	process = make_filter_map_test(lambda x: x % 2 == 1, lambda x: x * x)
+	print("Skapar en lista där varje position är x * x om x är ojämt, och x går från noll till n.")
+	n = int(input("Skriv nummer för n: "))
+	print(process(range(n+1)), sep ="\t")
 
-process = make_filter_map_test(lambda x: x % 2 == 1, lambda x: x * x)
-print(process(range(10)), 'test', sep='\t')
+if __name__ == "__main__":
+	main()
