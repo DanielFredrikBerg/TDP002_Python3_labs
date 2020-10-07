4a18f815ad21a85d56384854864b4ff2f8722e10
danhu849 <danhu849@gitlab.liu.se>
2020-10-07 16:31:59 +0200

    Binary_Search almost done, just need to implement function

--- lab6_danhu849_jenoh242.py
+++ lab6_danhu849_jenoh242.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 
 # Linjärsökning
+
+
 def linear_search(lista, value, func=None):
     if func == None:
         result = [item for item in lista if item == value]
@@ -11,23 +13,34 @@ def linear_search(lista, value, func=None):
                 return item
         return None
 
-def binary_search():
-    pass
-        
+
+def binary_search(sorted_list, needle, func=None):
+    minst = 0
+    högst = len(sorted_list)
+    average = int(högst/2)
+    print(sorted_list[average])
+    if sorted_list[average] == needle:
+        return sorted_list[average]
+    elif sorted_list[average] < needle:
+        return binary_search(sorted_list[average:högst], needle, func)
+    elif sorted_list[average] > needle:
+        return binary_search(sorted_list[minst:average], needle, func)
+
+
 def main():
-    a_list = [1,2,3,4,5,6,7]
-    b = linear_search(a_list, 2)
-    c = linear_search(a_list, 16)
-    print(b, c, sep='\t')
-
-    imdb_list = [{'title': 'Professional Help', 'actress': 'Big Chungus', 'score': 2}, {'title': 'Raising an error', 'actress': 'Big Cobra', 'score': 5}, {'title': 'Raise your voice', 'actress': 'Hilary Duff', 'score': 10}]
-    chungus = linear_search(imdb_list, 'Big Cobra', func=lambda e: e['actress'])
-    ph =  linear_search(imdb_list, 'Professional Help', func=lambda e: e['title'])
-    not_found = linear_search(imdb_list, 'Something Else', func=lambda e: e['title'])
-    print(chungus, ph, not_found, sep='\n')
-
-
-
+    # Linear Search
+    a_list = [num for num in range(100)]
+    # b = linear_search(a_list, -1)
+    # c = linear_search(a_list, 16)
+    # print(b, c, sep='\t')
+    # imdb_list = [{'title': 'Professional Help', 'actress': 'Big Chungus', 'score': 2}, {'title': 'Raising an error', 'actress': 'Big Cobra', 'score': 5}, {'title': 'Raise your voice', 'actress': 'Hilary Duff', 'score': 10}]
+    # chungus = linear_search(imdb_list, 'Big Cobra', func=lambda e: e['actress'])
+    # ph =  linear_search(imdb_list, 'Professional Help', func=lambda e: e['title'])
+    # not_found = linear_search(imdb_list, 'Something Else', func=lambda e: e['title'])
+    # print(chungus, ph, not_found, sep='\n')
+    # Binary Search
+    bs_res = binary_search(a_list, 70)
+    print('BS: ', bs_res)
     
 if __name__ == '__main__':
     main()
