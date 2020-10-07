da5fbe9dc52991c8796ad41f01e5f2460018ef6e
danhu849 <danhu849@gitlab.liu.se>
2020-10-07 12:09:47 +0200

    Halvvägs på linjärsökningen

--- lab6_danhu849_jenoh242.py
+++ lab6_danhu849_jenoh242.py
@@ -1,1 +1,20 @@
 #!/usr/bin/python3
+
+# Linjärsökning
+def linear_search(lista, value, func=None):
+    if func == None:
+        result = [item for item in lista if item == value]
+        return result[0]
+    else:
+        for i in range(0, len(lista)):
+            
+        
+                
+
+
+def main():
+    
+    
+if __name__ == '__main__':
+    main()
+
