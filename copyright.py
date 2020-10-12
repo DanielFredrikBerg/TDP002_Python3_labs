d57b681376eb0c1856b90e1787f6300643bed24f
danhu849 <danhu849@gitlab.liu.se>
2020-10-12 15:45:30 +0200

    copyright now works with both flags if specified

--- copyright.py
+++ copyright.py
@@ -12,6 +12,7 @@ def main():
         file_dest = sys.argv[2]
     except Exception:
         print("Please specify BOTH copyright info file and file destination.")
+    
     if len(sys.argv) == 5:
         if str(sys.argv[3]) == '-c':
             check_file_type = str(sys.argv[4])
@@ -20,6 +21,17 @@ def main():
                 new_file_name = file_dest.split('.')[0] + '.' + str(sys.argv[4])
             else:
                 new_file_name = file_dest + str(sys.argv[4])
+    elif len(sys.argv) == 7:
+        for argument in range(3,6,2):
+            if str(sys.argv[argument]) == '-c':
+                check_file_type = str(sys.argv[argument + 1])
+            elif str(sys.argv[argument]) == '-u':
+                if file_dest.__contains__('.'):
+                    new_file_name = file_dest.split('.')[0] + '.' + str(sys.argv[argument + 1])
+                else:
+                    new_file_name = file_dest + str(sys.argv[argument + 1])
+        
+        
 
     is_file = True
     if str(file_dest)[-1] == '/':
@@ -30,13 +42,11 @@ def main():
             copyright_text = cr_file.read()
 
         with open(str(file_dest), 'r') as destination_f:
-            dest_content = destination_f.read()
-    #else:
-        
+            dest_content = destination_f.read()        
 
     rewritten = re.sub(r'(?<=BEGIN COPYRIGHT)([\S\s]*?)(?=END COPYRIGHT)', copyright_text, dest_content)
 
-    print(new_file_name)
+    
     if check_file_type:
         if file_dest[-len(check_file_type):] == check_file_type:
             if new_file_name:
@@ -50,7 +60,7 @@ def main():
     elif new_file_name:
         with open(str(new_file_name), 'w') as rw_file:
             rw_file.write(rewritten)
-    else:
+    elif len(sys.argv) == 3:
         with open(str(file_dest), 'w') as rw_file:
             rw_file.write(rewritten)
         
